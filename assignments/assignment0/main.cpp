e5219fc4281dbaa93648231e2e8c63a2809f656b
Benjamin Politzer <benjamin.politzer@mymail.champlain.edu>
2025-01-27 17:07:48 -0500

    Initial progress on Assignment 0. Still need to do lighting and finish ImGui controls.

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -8,6 +8,13 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <ew/shader.h>
+#include <ew/camera.h>
+#include <ew/model.h>
+#include <ew/transform.h>
+#include <ew/cameraController.h>
+#include <ew/texture.h>
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
 void drawUI();
@@ -15,12 +22,38 @@ void drawUI();
 //Global state
 int screenWidth = 1080;
 int screenHeight = 720;
+int textures = 0;
 float prevFrameTime;
 float deltaTime;
+
+
+ew::Camera camera;
+ew::Transform monkeyTransform;
+ew::CameraController cameraController;
 
 int main() {
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	
+	//Texture list
+	GLuint brickTexture = ew::loadTexture("assets/brick_color.jpg"); 
+	GLuint woodTexture = ew::loadTexture("assets/wood_color.jpg");
+	GLuint goldTexture = ew::loadTexture("assets/gold_color.jpg");
+
+	glActiveTexture(GL_TEXTURE0);
+	//Bind default texture
+	glBindTexture(GL_TEXTURE_2D, brickTexture);
+
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Model monkeyModel = ew::Model("assets/suzanne.obj");
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f); //Look at the center of the scene
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f; //Vertical field of view, in degrees
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK); //Back face culling
+	glEnable(GL_DEPTH_TEST); //Depth testing
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -29,35 +62,78 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
+		GLuint currentTexture;
+
+		if (textures == 0) {
+			currentTexture = brickTexture;
+		}
+		else if (textures == 1) {
+			currentTexture = woodTexture;
+		}
+		else {
+			currentTexture = goldTexture;
+		}
+		glBindTexture(GL_TEXTURE_2D, currentTexture);
+
 		//RENDER
 		glClearColor(0.6f,0.8f,0.92f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		shader.use();
+		shader.setInt("_MainTex", 0);
+		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+		shader.setMat4("_Model", monkeyTransform.modelMatrix());
+		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
+		monkeyModel.draw(); //Draws monkey model using current shader
+
+		cameraController.move(window, &camera, deltaTime);
+
 		drawUI();
+
 
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
 }
+void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
+	camera->position = glm::vec3(0, 0, 5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+}
 
+void changeTexture() {
+	textures++;
+	if (textures > 2) {
+		textures = 0;
+	}
+}
 void drawUI() {
 	ImGui_ImplGlfw_NewFrame();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui::NewFrame();
 
 	ImGui::Begin("Settings");
-	ImGui::Text("Add Controls Here!");
+	if (ImGui::Button("Reset Camera")) {
+		resetCamera(&camera, &cameraController);
+	}
+	if (ImGui::Button("Change Texture")) {
+		changeTexture();
+	}
+	//Add more camera settings here!
 	ImGui::End();
 
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
+
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 	screenWidth = width;
 	screenHeight = height;
+	camera.aspectRatio = (float)screenWidth / screenHeight;
 }
 
 /// <summary>
